db81fa3358374967949996e23b3694a8dc0ec398
joanafonsogomes <afonnso.gommes@gmail.com>
2021-06-20 12:16:57 +0100

    progress auth

--- auth/app.js
+++ auth/app.js
@@ -21,7 +21,7 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error...'));
 db.once('open', function() {
-    console.log("MongoDB successful connection")
+    console.log("\nSuccessfully connected to MongoDB!")
 });
 
 // ROUTES
